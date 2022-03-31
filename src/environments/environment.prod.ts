40b60e5078b441a1beeb40e637da5b1a4a6b9dc8
Matt Payne <Payne@MattPayne.org>
2022-03-30 19:57:42 -0500

    ng add @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'webprojects-38489',
+    appId: '1:119800746358:web:dbc915163dfde59611ea57',
+    storageBucket: 'webprojects-38489.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBY0QyEVIG4fQfsJM-ickRS1zcl-qINaMk',
+    authDomain: 'webprojects-38489.firebaseapp.com',
+    messagingSenderId: '119800746358',
+    measurementId: 'G-FY470GHCLE',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'webprojects-38489',
+    appId: '1:119800746358:web:dbc915163dfde59611ea57',
+    storageBucket: 'webprojects-38489.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBY0QyEVIG4fQfsJM-ickRS1zcl-qINaMk',
+    authDomain: 'webprojects-38489.firebaseapp.com',
+    messagingSenderId: '119800746358',
+    measurementId: 'G-FY470GHCLE',
+  },
   production: false
 };
 
